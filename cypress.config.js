37ab9fd2b693f8a9c218c13210efe3bcf227dc76
l.vianello <l.vianello@higecomore.com>
2026-07-01 11:48:37 +0200

    fix: job folders

--- cypress.config.js
+++ cypress.config.js
@@ -3,7 +3,7 @@ import { defineConfig } from 'cypress'
 export default defineConfig({
   e2e: {
     baseUrl: 'http://localhost:8008',
-    specPattern: 'cypress/e2e/**/*.cy.{js,ts}',
+    specPattern: 'cypress/**/**/*.cy.{js,ts}',
     supportFile: false
   }
 })
