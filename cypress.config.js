458f491f02a917639f18486f06bdd9f21dcfdcf8
l.vianello <l.vianello@higecomore.com>
2026-07-01 11:46:18 +0200

    fix: baseurl

--- cypress.config.js
+++ cypress.config.js
@@ -2,7 +2,7 @@ import { defineConfig } from 'cypress'
 
 export default defineConfig({
   e2e: {
-    baseUrl: 'http://localhost:5173',
+    baseUrl: 'http://localhost:8008',
     specPattern: 'cypress/e2e/**/*.cy.{js,ts}',
     supportFile: false
   }
